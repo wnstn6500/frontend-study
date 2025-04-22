d4ff1b36537c3a92823618187323c7a7e1d2205a
junsu <wnstn6500@naver.com>
2025-04-22 18:26:32 +0900

    3주차 화요일 수업

--- 08_portfolio-site/js/main.js
+++ 08_portfolio-site/js/main.js
@@ -12,3 +12,27 @@ spyEls.forEach(function (spyEl) {
   .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
   .addTo(controller); // 컨트롤러에 장면을 할당(필수!) -라이브러리에서 지정한 문법으로 깊게 이해x
 });
+
+// Swipqer 사용
+const swiper = new Swiper('.project .swiper', {
+  // 슬라이드 옵션 지정
+  // direction : 'vertical', // 수직 슬라이드
+  direction: 'horizontal', //수평 슬라이드(기본값)
+  loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 다시 1
+  autoplay: { // 자동 재생 여부
+    delay: 5000 // 5초마다 슬라이드 바뀜(기본값: 3000)
+  },
+
+  // 페이지네이션 옵션
+  pagination: {
+    el: '.project .swiper-pagination',
+    clickable: true // 사용자의 페이지네이션 요소 제어 가능 여부
+  },
+
+  // 이전/다음 슬라이드 버튼 옵션
+  navigation: {
+    nextEl: '.project .swiper-button-next',
+    prevEl: '.project swiper-button-prev',
+  },
+
+});
